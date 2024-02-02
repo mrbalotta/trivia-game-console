a8f2e4ceea002944f63ce554be84f82dee126f29
Alessandro Balotta <>
2024-02-02 10:06:29 -0300

    amend

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import * as providers from './providers'
-
-(function main() {
-    for(const provider in providers) {
-        const entrypoint = providers[provider]
-        if(entrypoint instanceof Function) entrypoint()
-    }
-})()
